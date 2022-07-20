dcc3f7f8781e9ecc4f2c5ac6c8672cebaebd77aa
meikalaheshla <fausto.boshoff@gmail.com>
2022-07-20 16:16:16 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let root = new Vue({
+    el: '#root',
+    data: {
+        mails: [],
+        endpoint: 'https://flynn.boolean.careers/exercises/api/random/mail',
+
+    },
+    methods: {
+    }, 
