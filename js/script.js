8fb222d1db5ed9e5ca7c5f0fd4fe617422399b5d
meikalaheshla <fausto.boshoff@gmail.com>
2022-07-20 16:17:50 +0200

    stampate le mail in pagina

--- js/script.js
+++ js/script.js
@@ -6,4 +6,17 @@ let root = new Vue({
 
     },
     methods: {
-    }, 
+    }, created() {
+        for (i = 1; i <= 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((res) => {
+
+                    this.mails.push(res.data.response)
+
+
+                    console.log(this.mails)
+                })
+        }
+    }
+
+})
